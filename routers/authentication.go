6c77fd34b365d104fd106f97d02433b148b3c3af
DmitryBugrov <dmitrybugrov@gmail.com>
2016-04-21 02:29:18 +0300

    Update authentication.go

--- routers/authentication.go
+++ routers/authentication.go
@@ -9,11 +9,6 @@ import (
 
 func SetAuthenticationRoutes(router *mux.Router) *mux.Router {
 	router.HandleFunc("/api/v1/token-auth", controllers.Login).Methods("POST")
-	router.Handle("/api/v1/refresh-token-auth",
-		negroni.New(
-			negroni.HandlerFunc(authentication.RequireTokenAuthentication),
-			negroni.HandlerFunc(controllers.RefreshToken),
-		)).Methods("GET")
 
 	return router
 }
